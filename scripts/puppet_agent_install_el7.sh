e25efc23edb5c1b0422f76561853b46cc3d82dff
zoojar <davidnewton82@gmail.com>
2016-11-09 17:11:42 +0000

    r10k setup optional

--- scripts/puppet_agent_install_el7.sh
+++ scripts/puppet_agent_install_el7.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Script to install puppet agent on el7
+# Pass optional argument for a url to download r10k.yaml
 
 log_file='/var/log/puppet_agent_install_el7.sh.log'
 exec > >(tee -a $log_file) 2>&1
+
+r10k_yaml_url=$1
 
 puppet_release_package='https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm'
 puppet_path='/opt/puppetlabs/puppet/bin'
@@ -12,8 +15,14 @@ echo "$(date) INFO: Installing puppet..."
 sudo rpm -Uvh $puppet_release_package
 export PATH=$puppet_path:$PATH
 sudo yum install -y $puppet_package 
-$puppet_path/gem install r10k
-yum -y install git
-cd /etc/puppetlabs/puppet
-curl https://raw.githubusercontent.com/zoojar/control-repo-ccc/production/r10k.yaml > r10k.yaml
-r10k deploy environment production -v
+
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+
+if [[ $r10k_yaml_url =~ $regex ]] ; then 
+  echo "$(date) INFO: Installing r10k and running a deploy from ${r10k_yaml_url}..."
+  $puppet_path/gem install r10k
+  yum -y install git
+  cd /etc/puppetlabs/puppet
+  curl $r10k_yaml_url > r10k.yaml
+  r10k deploy environment production -v
+fi
