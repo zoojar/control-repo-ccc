48b56f758201777a0ca8db305fc09cafc8bb8d33
zoojar <davidnewton82@gmail.com>
2016-11-18 11:53:34 +0000

    r10k prep -

--- spec/acceptance/example_spec.rb
+++ spec/acceptance/example_spec.rb
@@ -4,20 +4,8 @@ describe 'file test' do
 
   context 'default parameters' do
     # Using puppet_apply as a helper
-    it 'should work with no errors based on the example' do
-      pp = <<-EOS
-        include role::webserver
-      EOS
-
-      # Run it twice and test for idempotency
-      #expect(apply_manifest(pp).exit_code).to_not eq(1)
-      #expect(apply_manifest(pp).exit_code).to eq(0)
-
-      describe file('/etc/puppetlabs/code/environments/production/site/manifests/role/webserver.pp') do
-        it { should be_file }
-      end
-
+    describe file('/etc/puppetlabs/code/environments/production/site/manifests/role/webserver.pp') do
+      it { should be_file }
     end
-
   end
 end
