98a249c12c667c4b9caca94239328619a2e99361
zoojar <davidnewton82@gmail.com>
2016-11-18 10:55:36 +0000

    r10k prep -

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -8,6 +8,21 @@ UNSUPPORTED_PLATFORMS = ['windows','Solaris','Darwin']
 # Determine control repo url and branch from jenkins env vars...
 R10K_REMOTE           = "#{ENV['GIT_URL']}" 
 R10K_BRANCH           = "#{"#{ENV['GIT_BRANCH']}".slice! "origin/"}"
+r10k_pp = <<EOD
+class { 'r10k':
+  sources           => {
+    'puppet' => {
+        'remote'  => '#{R10K_REMOTE}',
+        'basedir' => "\${::settings::confdir}/environments",
+        'prefix'  => false,
+    }
+  },
+  purgedirs         => ["\${::settings::confdir}/environments"],
+  manage_modulepath => false,
+}
+EOD
+
+
 
 RSpec.configure do |c|
   # Project root
@@ -21,9 +36,9 @@ RSpec.configure do |c|
     # Install module and dependencies
     hosts.each do |host|
       on host, puppet('module', 'install', 'zack-r10k', '--version', '3.2.0' ), { :acceptable_exit_codes => [0] }
-      pp = "\"class { \'r10k\': remote => \'#{R10K_REMOTE}\', } \""
-      on host, puppet('apply', '-e', pp)
-      on host, "r10k deploy --config /etc/r10k.yaml environment -p -v"
+      create_remote_file(host, '/tmp/r10k.pp', r10k_pp)
+      on host, puppet('apply', '/tmp/r10k.pp')
+      on host, "r10k deploy environment -p -v"
     end
   end
 end
